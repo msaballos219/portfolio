da71462c4c2083d93e02533519f4dff6c2ae11bc
Melvin <msaballos219@gmail.com>
2025-02-06 14:41:07 -0600

    build changes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
 
-import netlify from "@astrojs/netlify";
-
-// https://astro.build/config
 export default defineConfig({
-  output:'server',
+  output: "static",
   integrations: [mdx(), sitemap(), tailwind(), icon()],
-  adapter: netlify(),
+  adapter: cloudflare(), // Removed mode and functionPerRoute options
 });
